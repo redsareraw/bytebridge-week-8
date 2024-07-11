5c3606af869a0771e8fb265bedbe81eaa38a2dab
redsareraw <dmarshall@musfiber.net>
2024-07-10 20:53:45 -0400

    Created two new files: index.html and script.js. The index links the script file and the script file creates a function that multiplies two numbers. There is an alert if the number is less than 2000 or greater than 2000.

---
+++ lesson-8-2/script.js
@@ -0,0 +1,13 @@
+function numberCalculation() {
+    let number1 = 45;
+    let number2 = 78;
+    let multiplyNumbers = number1 * number2;
+
+    if (multiplyNumbers < 2000) {
+        alert("I wish it was a bigger number");
+    } else {
+        alert("That's more like it!");
+    }
+}
+
+numberCalculation();
